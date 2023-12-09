b92fcb2d26342148e26cbeb77d3b2349693875c8
Zhm17 <zhemo17@gmail.com>
2023-12-08 19:52:15 -0600

    - Point and click function

--- Assets/Scripts/CameraRaycastAim.cs
+++ Assets/Scripts/CameraRaycastAim.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class CameraRaycastAim : MonoBehaviour
 {
+    public delegate void TargetSpawnedAction(Transform t);
+    public static event TargetSpawnedAction OnTargetSpawned;
+
     private Camera MainCamera;
     [SerializeField]
     private Transform _3dPointer;
+
+    [Header("Target / Spawn")]
+    [SerializeField]
+    public Transform _targetAreaT;
 
     private void Start()
     {
@@ -13,29 +20,10 @@ public class CameraRaycastAim : MonoBehaviour
 
     private void Update()
     {
-        RaycastHit hit;
-        Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
-
-        int layerMask = 1 << 8;
-        layerMask = ~layerMask;
-
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
-        {
-            //Debug.Log(hit.transform.name);
-            if (hit.transform.CompareTag("Ground"))
-            {
-                Debug.DrawRay(transform.position, ray.direction * hit.distance, Color.yellow, 0.01f);
-                _3dPointer.position = new Vector3(hit.point.x,_3dPointer.position.y,hit.point.z);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            ShootClick();
-        }
+        PointNClick();
     }
 
-    private void ShootClick()
+    private void PointNClick()
     {
         RaycastHit hit;
         Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
@@ -48,9 +36,34 @@ public class CameraRaycastAim : MonoBehaviour
             //Debug.Log(hit.transform.name);
             if (hit.transform.CompareTag("Ground"))
             {
-                // Spawn cube
-                Debug.Log("Shooting");
+                // Ray Debug
+                Debug.DrawRay(transform.position, ray.direction * hit.distance, Color.yellow, 0.01f);
+
+                //Move 
+                if (_3dPointer)
+                    _3dPointer.position = new Vector3(hit.point.x, _3dPointer.position.y, hit.point.z);
+
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    ShootClick(hit.point);
+                }
+
             }
+        }
+    }
+
+    // Spawn destionation points
+    private void ShootClick(Vector3 newPos)
+    {
+        // Spawn cube
+        Debug.Log("Shooting");
+
+        if (_targetAreaT)
+        {
+            _targetAreaT.position = new Vector3(newPos.x, _targetAreaT.position.y, newPos.z);
+
+            if(OnTargetSpawned!=null) 
+                OnTargetSpawned(_targetAreaT);
         }
     }
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,10 +7,31 @@ public class Unit : MonoBehaviour
     public Transform targetPosition;
     NavMeshAgent agent => GetComponent<NavMeshAgent>();
 
+    #region EVENTS SUSCRIPTIONS
+    private void OnEnable()
+    {
+        CameraRaycastAim.OnTargetSpawned += GetTarget;
+    }
+
+    private void OnDisable()
+    {
+        CameraRaycastAim.OnTargetSpawned -= GetTarget;
+    }
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        agent.destination = targetPosition!.position;
+        if(targetPosition) 
+            agent.SetDestination(targetPosition.position);
+    }
+
+    private void GetTarget(Transform t)
+    {
+        if(t == null)
+            return;
+
+        agent.destination = t.position;
     }
 
 }
